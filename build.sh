8249924a2c52b95be53eac4fa7ca99ea6957985a
Wojtek Mach <wojtek@wojtekmach.pl>
2015-12-30 15:44:20 +0100

    debug

--- build.sh
+++ build.sh
@@ -4,10 +4,12 @@ set -e
 
 echo "building for OSX..."
 GOOS=darwin go build
+ls
 mv catd bin/catd_mac
 echo "done."
 
 echo "building for Linux..."
 GOOS=linux go build
+ls
 mv catd bin/catd_linux
 echo "done."
